e6a5f4fc29cdac944e95475d84efd1dd15bb4b1d
Priyesh <safal.priyesh@gmail.com>
2020-07-21 11:00:47 -0400

    first try with graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+
+
+const fetch = require(`node-fetch`)
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // get data from GitHub API at build time
+  const result = await fetch(`https://api.github.com/users/priyesh18/repos`)
+  const resultData = await result.json()
+  // create node for build time data example in the docs
+  createNode({
+    // nameWithOwner and url are arbitrary fields from the data
+    repos: resultData,
+
+    // required fields
+    id: `github`,
+    parent: null,
+    children: [],
+    internal: {
+      type: `github`,
+      contentDigest: createContentDigest(resultData),
+    },
+  })
+}
--- src/partials/allProjects.component.jsx
+++ src/partials/allProjects.component.jsx
@@ -3,9 +3,25 @@ import "./allProjects.styles.scss"
 import ProjectCard from "../components/project-card/project-card.component"
 
 import ProjectData from "../data/projects.json"
+import { graphql, useStaticQuery } from "gatsby"
 
-const AllProjects = () => (
-  <div className="all-projects">
+const AllProjects = () => {
+  // const gatsbyRepoData = useStaticQuery(graphql`
+  //   query {
+  //     github {
+        
+  //         id
+  //         nameWithOwner
+  //         url
+        
+  //     }
+  //   }
+  // `)
+
+  return (
+    
+    <div className="all-projects">
+    {/* {console.log(gatsbyRepoData)} */}
     {ProjectData.map((project) => (
           <ProjectCard 
             key={project.id} 
@@ -14,6 +30,7 @@ const AllProjects = () => (
           />
         ))}
   </div>
-)
+  )
+}
 
 export default AllProjects
